88903090fa3bcf842487f2bcdf35b00b7b8bc8ac
Tefaia Edmondson <2010tefaia@gmail.com>
2022-11-11 09:39:35 -0800

    online store inital commit

---
+++ sanityecom/schemas/banner.js
@@ -0,0 +1,115 @@
+export default {
+    name: 'banner',
+    title: 'Banner',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: 'buttonText',
+            title: 'ButtonText',
+            type: 'string',
+        },
+        {
+            name: 'product',
+            title: 'Product',
+            type: 'string',
+        },
+        {
+            name: 'desc',
+            title: 'Desc',
+            type: 'string',
+        },
+        {
+            name: 'smallText',
+            title: 'SmallText',
+            type: 'string',
+        },
+        {
+            name: 'midText',
+            title: 'MidText',
+            type: 'string',
+        },
+        {
+            name: 'largeText1',
+            title: 'LargeText1',
+            type: 'string',
+        },
+        {
+            name: 'largeText2',
+            title: 'LargeText2',
+            type: 'string',
+        },
+        {
+            name: 'discount',
+            title: 'Discount',
+            type: 'string',
+        },
+        {
+            name: 'saleTime',
+            title: 'SaleTime',
+            type: 'string',
+        },
+
+        // for banner 2
+        {
+            name: 'image2',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: 'b2buttonText',
+            title: 'b2ButtonText',
+            type: 'string',
+        },
+        {
+            name: 'b2product',
+            title: 'b2Product',
+            type: 'string',
+        },
+        {
+            name: 'b2desc',
+            title: 'b2Desc',
+            type: 'string',
+        },
+        {
+            name: 'b2smallText',
+            title: 'b2SmallText',
+            type: 'string',
+        },
+        {
+            name: 'b2midText',
+            title: 'b2MidText',
+            type: 'string',
+        },
+        {
+            name: 'b2largeText1',
+            title: 'b2LargeText1',
+            type: 'string',
+        },
+        {
+            name: 'b2largeText2',
+            title: 'b2LargeText2',
+            type: 'string',
+        },
+        {
+            name: 'b2discount',
+            title: 'b2Discount',
+            type: 'string',
+        },
+        {
+            name: 'b2saleTime',
+            title: 'b2SaleTime',
+            type: 'string',
+        },
+    ],
+  };
